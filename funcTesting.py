217a965cc8e1e33e999661061d56d2f52ff954bf
Lauren Geislinger <72772740+LaurenGeis@users.noreply.github.com>
2021-06-16 15:03:47 -0700

    Add files via upload

---
+++ funcTesting.py
@@ -0,0 +1,9 @@
+def sayHello(name): #this is defining a function, creates a function
+    print ('Hello ' + name)
+sayHello('Al') #this is the line that calls the newly created function. Sends the execution to the top of the functions code
+#passing the string value 'Al' into the function.
+#a value being passed into a function in a function call is an argument.
+#the arguement 'Al' is assigned to local variable, name.
+#variables that have arguments assigned to them are parameters.
+
+
